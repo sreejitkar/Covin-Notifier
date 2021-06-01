f3415e90654645ee797d3cf7b3c7b0863a1d7993
Sreejit Kar <sreejitkar999@gmail.com>
2021-06-01 11:17:05 +0530

    nodemon sus changed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Covid vaccine app listening at http://%s:%s", host, port)
  })
 var email_service = true;
 
